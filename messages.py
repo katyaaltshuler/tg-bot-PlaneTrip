bf2590c997d03922a44e6746f2afeacbfc77bbf8
Katerina <katya.python.info@gmail.com>
2022-04-01 11:21:21 +0200

    Messages Commit

--- messages.py
+++ messages.py
@@ -9,12 +9,12 @@ class Messages:
                                ' </b>'
         self.ask_round_date_interval = 'Type a desired date interval where you plan a Trip, min and max amount  ' \
                                        'of desirable days of stay in the following mode:' \
-                                       ' \n\n<b>24/03 - 08/07 min 7 max 12 days</b>'
+                                       ' \n\n<b>24/04 - 08/07 min 7 max 12 days</b> \n or\n 20/12/2022 - 08/01/2023 min 3 max 5 days'
         self.ask_oneway_date_interval = 'Type a desired date interval where you plan a Trip in the following mode:' \
-                                        ' \n\n<b>24/03 - 08/07</b>'
+                                        ' \n\n<b>24/03 - 08/07 \n or\n 20/05/2022 - 08/06/2023</b>'
 
-        self.ask_price = f'Price up to .. EUR/GBP/USD:' \
-                         f'\n\n 50 euro'
+        self.ask_price = f'Price up to ... EUR/GBP/USD, for example:' \
+                         f'\n\n <b>50 euro or 65 usd</b>'
         self.unsuccessful_result = 'There is no interesting flight for you at this moment, ' \
                                    'try another /search_destination or check this trip a little bit later 🥝'
         self.denied = 'Sorry, Access Denied'
