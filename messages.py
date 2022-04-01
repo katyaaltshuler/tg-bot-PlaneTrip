7ae74435cacdeb58d8e307560be86572eaa54bd9
Katerina <katya.python.info@gmail.com>
2022-04-01 09:48:31 +0200

    Refactoring

--- messages.py
+++ messages.py
@@ -7,11 +7,14 @@ class Messages:
                                'Mykonos </b>\n or \n<b>oneway 1 change Berlin to London </b>\n or \n<b>round ' \
                                'direct Milan to anywhere' \
                                ' </b>'
-        self.ask_round_date_interval = 'Type a desired date, min and max amount days in the following mode:' \
+        self.ask_round_date_interval = 'Type a desired date interval where you plan a Trip, min and max amount  ' \
+                                       'of desirable days of stay in the following mode:' \
                                        ' \n\n<b>24/03 - 08/07 min 7 max 12 days</b>'
-        self.ask_oneway_date_interval = 'Type a desired date in the following mode: \n\n<b>24/03 - 08/07</b>'
+        self.ask_oneway_date_interval = 'Type a desired date interval where you plan a Trip in the following mode:' \
+                                        ' \n\n<b>24/03 - 08/07</b>'
 
-        self.ask_price = f'Price up to .. EUR/GBP/USD'
+        self.ask_price = f'Price up to .. EUR/GBP/USD:' \
+                         f'\n\n 50 euro'
         self.unsuccessful_result = 'There is no interesting flight for you at this moment, ' \
                                    'try another /search_destination or check this trip a little bit later 🥝'
         self.denied = 'Sorry, Access Denied'
